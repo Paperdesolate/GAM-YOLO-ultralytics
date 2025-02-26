b2511be33f3fb8f6bfdfcadc52b634edaabfb64a
DORTORLIU <155125727+DORTORLIU@users.noreply.github.com>
2025-02-26 22:44:11 +0800

    Update mytrain.py

--- ultralytics/mytrain.py
+++ ultralytics/mytrain.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
 
 
 if __name__ == '__main__':
-    model = YOLO('D:/ultralytics/ultralytics/cfg/models/v8/yolov8-seg.yaml')
-    model = YOLO('D:/ultralytics/yolov8m-seg.pt')
+    model = YOLO(':.yaml')
+    model = YOLO('.pt')
 
-    model.train(data='D:/ultralytics/ultralytics/cfg/datasets/coco8-seg.yaml')
+    model.train(data='.yaml')
